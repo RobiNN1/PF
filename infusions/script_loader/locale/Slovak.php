10264ef750826af051bc83e2f1dc74bd2f1cd0a8
RobiNN <kelcakrobo@gmail.com>
2021-03-03 14:13:35 +0100

    Add script loader locales

---
+++ infusions/script_loader/locale/Slovak.php
@@ -0,0 +1,24 @@
+<?php
+/*-------------------------------------------------------+
+| PHPFusion Content Management System
+| Copyright (C) PHP Fusion Inc
+| https://phpfusion.com/
++--------------------------------------------------------+
+| Filename: Slovak.php
+| Author: RobiNN
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['scl_title'] = 'Script Loader';
+$locale['scl_head'] = 'Hlavička';
+$locale['scl_head_desc'] = 'Kód, ktorý sa musí načítať z hlavičky (pred &lt;/head&gt; tagom)';
+$locale['scl_footer'] = 'Päta';
+$locale['scl_footer_desc'] = 'Kód, ktorý sa musí načítať z päty (pred &lt;/body&gt; tagom)';
+$locale['scl_code'] = 'Kód';
+$locale['scl_notice'] = 'Kód bol uložený.';
