258f4e7c89b6cb4e59cf28aae1406482c8659264
RobiNN <kelcakrobo@gmail.com>
2021-03-20 22:05:33 +0100

    Add locales

---
+++ infusions/videos/locale/German/search/videos.php
@@ -0,0 +1,23 @@
+<?php
+/*-------------------------------------------------------+
+| PHPFusion Content Management System
+| Copyright (C) PHP Fusion Inc
+| https://phpfusion.com/
++--------------------------------------------------------+
+| Filename: videos.php
+| Author: sala99
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['videos.php'] = 'Videos';
+$locale['v400'] = 'Videos';
+$locale['v401'] = 'Video';
+$locale['v402'] = 'Videos';
+$locale['v403'] = 'Datum hinzugefügt:';
+$locale['v404'] = 'Ansichten:';
---
+++ infusions/videos/locale/German/videos.php
@@ -0,0 +1,121 @@
+<?php
+/*-------------------------------------------------------+
+| PHPFusion Content Management System
+| Copyright (C) PHP Fusion Inc
+| https://phpfusion.com/
++--------------------------------------------------------+
+| Filename: videos.php
+| Author: sala99
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['vid_title'] = 'Videos';
+$locale['vid_desc'] = 'Ein Standardvideosystem.';
+$locale['vid_latest'] = 'Neueste Videos';
+$locale['vid_submit'] = 'Video senden';
+$locale['video_submit'] = '%s Videos';
+$locale['vid_001'] = 'Kategorien';
+$locale['vid_002'] = 'Einreichung';
+$locale['vid_003'] = 'Einstellungen';
+$locale['vid_004'] = 'Es sind keine Videokategorien definiert. <br/> Sie müssen mindestens eine Kategorie definieren, bevor Sie Videos hinzufügen können.';
+$locale['vid_005'] = 'Alle Videos';
+$locale['vid_006'] = 'Zeigt derzeit %d von %d der gesamten Videoeinträge an';
+$locale['vid_007'] = 'Filterkategorie anzeigen nach';
+$locale['vid_008'] = 'Es wurden keine Videos hinzugefügt';
+$locale['vid_009'] = 'Kategorie';
+$locale['vid_010'] = 'Video Title';
+$locale['vid_011'] = 'Videotitel darf nicht leer gelassen werden.';
+$locale['vid_012'] = 'Video Keywords';
+$locale['vid_013'] = 'Geben Sie die einzelnen Schlüsselwörter ein und drücken Sie die Eingabetaste, um weitere hinzuzufügen';
+$locale['vid_013a'] = 'Video länge';
+$locale['vid_013b'] = 'Die Videolänge darf nicht leer bleiben.';
+$locale['vid_014'] = 'Video beschreibung';
+$locale['vid_015'] = 'Video Screenshot';
+$locale['vid_015a'] = 'Wenn das Video von YouTube / Vimeo stammt, wird der Screenshot nicht benötigt.';
+$locale['vid_016'] = 'Max. Dateigröße: %s / Zulässige Dateitypen: %s / Max Breite: %spx, Max. Höhe: %spx';
+$locale['vid_017'] = 'Videoquelle: Bitte geben Sie entweder eine Datei, eine URL oder einen Einbettungscode an';
+$locale['vid_017a'] = 'Video Type';
+$locale['vid_018'] = 'Datei';
+$locale['vid_019'] = 'URL';
+$locale['vid_019a'] = 'Quelle';
+$locale['vid_019b'] = 'Verwenden Sie nur die Teile, die als <span class="required">red</span> hervorgehoben sind.';
+$locale['vid_020'] = 'Code einbetten';
+$locale['vid_021'] = 'Übermittelte Videodatei';
+$locale['vid_022'] = 'Videodatei erforderlich';
+$locale['vid_023'] = 'Max. Dateigröße: %s / Zulässige Dateitypen: %s';
+$locale['vid_024'] = 'Video-URL ist erforderlich';
+$locale['vid_025'] = 'Eingebetteter Video-Code ist erforderlich';
+$locale['vid_026'] = '%s sind für alle Elemente deaktiviert. Diese Einstellung wird nur wirksam, wenn sie wieder aktiviert werden.';
+$locale['vid_027'] = 'Sichtbarkeit';
+$locale['vid_028'] = 'Kommentare aktivieren';
+$locale['vid_029'] = 'Bewertungen aktivieren';
+$locale['vid_030'] = 'Veröffentlichungsdatum aktualisieren';
+$locale['vid_031'] = 'Video speichern';
+$locale['vid_032'] = 'Videokategorie kann nicht gelöscht werden <br/> Es sind Videos oder Unterkategorien mit dieser Kategorie verknüpft';
+$locale['vid_033'] = 'Kategoriename';
+$locale['vid_034'] = 'Bitte geben Sie einen Kategorienamen ein.';
+$locale['vid_035'] = 'Kategoriebeschreibung';
+$locale['vid_037'] = 'Kategoriesortierung';
+$locale['vid_038'] = 'Video ID';
+$locale['vid_039'] = 'Video datum';
+$locale['vid_040'] = 'Aufsteigend';
+$locale['vid_041'] = 'Absteigend';
+$locale['vid_042'] = 'Übergeordnete Kategorie';
+$locale['vid_043'] = 'Kategorie-Editor';
+$locale['vid_044'] = 'Aktuelle Kategorien';
+$locale['vid_045'] = 'Es sind keine Kategorien definiert';
+$locale['vid_046'] = 'Sie müssen die Datei löschen.';
+$locale['vid_047'] = 'Videoübermittlung wurde veröffentlicht';
+$locale['vid_048'] = 'Videoübermittlung wurde gelöscht';
+$locale['vid_049'] = 'Das Video wurde eingereicht von ';
+$locale['vid_050'] = 'Gepostet am ';
+$locale['vid_051'] = 'Veröffentlichung veröffentlichen';
+$locale['vid_052'] = 'Einreichung löschen';
+$locale['vid_053'] = 'Derzeit stehen %s für Ihre Überprüfung aus.';
+$locale['vid_054'] = 'Einreichung ID';
+$locale['vid_055'] = 'Einreichung Author';
+$locale['vid_056'] = 'Einreichung zeit';
+$locale['vid_057'] = 'Einreichungsgegenstand zur Überprüfung';
+$locale['vid_058'] = 'Derzeit sind keine Videoübermittlungen vorhanden';
+$locale['vid_059'] = 'Einstellungen für das Videosystem';
+$locale['vid_060'] = 'Videoelemente pro Seite';
+$locale['vid_061'] = 'Maximale Größe der Screenshot-Datei:';
+$locale['vid_062'] = 'Breite x Höhe';
+$locale['vid_063'] = 'Maximale Größe der Screenshot-Datei:';
+$locale['vid_064'] = 'Maximale Videogröße:';
+$locale['vid_065'] = 'Übermittlungen aktivieren?';
+$locale['vid_066'] = 'Zulässige Dateitypen:';
+$locale['vid_067'] = 'Videos Home';
+$locale['vid_068'] = 'Meist gesehen';
+$locale['vid_069'] = 'Neueste';
+$locale['vid_070'] = 'Die meisten Kommentare';
+$locale['vid_071'] = 'Best bewertet';
+$locale['vid_072'] = 'Keine Kategorie';
+$locale['vid_073'] = 'Autoren';
+$locale['vid_074'] = 'Kein Autor';
+$locale['vid_075'] = 'Es wurden noch keine Videos hinzugefügt';
+$locale['vid_076'] = 'Video senden';
+$locale['vid_077'] = 'Leider akzeptieren wir derzeit keine Videoübermittlungen auf dieser Seite.';
+$locale['vid_078'] = 'Unbekannter Fehler';
+$locale['vid_079'] = 'Danke, dass du dein Video eingereicht hast';
+$locale['vid_080'] = 'Ein weiteres Video senden';
+$locale['vid_081'] = 'Zurück zu [SITENAME]';
+$locale['vid_082'] = 'Verwenden Sie das folgende Formular, um ein Video einzureichen. <br/> Ihre Übermittlung wird von einem Administrator überprüft..
+[SITENAME] behält sich das Recht vor, Beiträge zu ändern oder zu bearbeiten.
+Videos sollten auf den Inhalt dieser Website anwendbar sein. Einsendungen, die als ungeeignet erachtet werden, werden abgelehnt.';
+$locale['vid_083'] = 'Likes/Dislikes aktivieren?';
+$locale['vid_notice_01'] = 'Video wurde aktualisiert';
+$locale['vid_notice_02'] = 'Video wurde hinzugefügt';
+$locale['vid_notice_03'] = 'Video wurde gelöscht';
+$locale['vid_notice_04'] = 'Videodatei, URL, Einbettungscode dürfen nicht leer gelassen werden';
+$locale['vid_notice_05'] = 'Kategorie wurde gelöscht';
+$locale['vid_notice_06'] = 'Kategorie wurde aktualisiert';
+$locale['vid_notice_07'] = 'Diese Kategorie existiert bereits';
+$locale['vid_notice_08'] = 'Kategorie wurde hinzugefügt';
+$locale['vid_notice_09'] = 'Einstellungen wurden aktualisiert';
