8e6ac52c9f4cba8251cec2179fb74e59c8710800
RobiNN <kelcakrobo@gmail.com>
2021-03-24 13:30:15 +0100

    Add locales

---
+++ infusions/script_loader/locale/French.php
@@ -0,0 +1,24 @@
+<?php
+/*-------------------------------------------------------+
+| PHPFusion Content Management System
+| Copyright (C) PHP Fusion Inc
+| https://phpfusion.com/
++--------------------------------------------------------+
+| Filename: French.php
+| Author: lechti64
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['scl_title'] = 'Script Loader';
+$locale['scl_head'] = 'Header';
+$locale['scl_head_desc'] = 'Code qui doit être chargé à partir de l\'en-tête (avant la balise &lt;/head&gt;)';
+$locale['scl_footer'] = 'Footer';
+$locale['scl_footer_desc'] = 'Code qui doit être chargé depuis le pied de page (avant la balise &lt;/body&gt;)';
+$locale['scl_code'] = 'Code';
+$locale['scl_notice'] = 'Le code a été enregistré.';
