2a7c756390c32bc7054f96f9481d464b20674627
RobiNN <kelcakrobo@gmail.com>
2021-07-10 12:22:44 +0200

    Create German.php

---
+++ infusions/script_loader/locale/German.php
@@ -0,0 +1,24 @@
+<?php
+/*-------------------------------------------------------+
+| PHPFusion Content Management System
+| Copyright (C) PHP Fusion Inc
+| https://phpfusion.com/
++--------------------------------------------------------+
+| Filename: German.php
+| Author: lupopa
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['scl_title'] = 'Script Loader';
+$locale['scl_head'] = 'Kopfbereich';
+$locale['scl_head_desc'] = 'Der Code kommt in den head Bereich also zwischen dem &lt;head&gt; &lt;/head&gt; tag)';
+$locale['scl_footer'] = 'Im Footer';
+$locale['scl_footer_desc'] = 'Der Code kommt vor den footer Bereich also erscheint vor dem &lt;/body&gt; tag)';
+$locale['scl_code'] = 'Fu&szlig;bereich';
+$locale['scl_notice'] = 'Der Code wurde gespeichert.';
