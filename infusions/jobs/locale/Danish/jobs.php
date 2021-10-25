412d17ad27ca243a5d093160b4f14483651efe2e
RobiNN <kelcakrobo@gmail.com>
2021-10-25 12:46:16 +0200

    Add Danish locales for jobs

---
+++ infusions/jobs/locale/Danish/jobs.php
@@ -0,0 +1,117 @@
+<?php
+/*-------------------------------------------------------+
+| PHPFusion Content Management System
+| Copyright (C) PHP Fusion Inc
+| https://phpfusion.com/
++--------------------------------------------------------+
+| Filename: jobs.php
+| Author: Helmuth Mikkelsen (helmuth)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['jb_title'] = 'Jobs';
+$locale['jb_desc'] = 'System til tilføjelse af job.';
+$locale['jb_100'] = 'Titel';
+$locale['jb_101'] = 'Kategori';
+$locale['jb_102'] = 'Placering';
+$locale['jb_103'] = 'FAQ';
+$locale['jb_104'] = 'Handlinger';
+$locale['jb_105'] = 'Der er ingen kategorier';
+$locale['jb_106'] = 'Jobs';
+$locale['jb_107'] = 'Kategorier';
+$locale['jb_108'] = 'Placering';
+$locale['jb_109'] = 'Ansøgere';
+$locale['jb_110'] = 'Indstillinger';
+$locale['jb_111'] = 'Der er ingen kategorier.<br/>Du skal definere mindst én kategori, før du kan tilføje jobs.';
+$locale['jb_112'] = 'Job er blevet slettet';
+$locale['jb_113'] = 'Job er blevet opdateret';
+$locale['jb_114'] = 'Job er blevet tilføjet';
+$locale['jb_115'] = 'FAQ er blevet slettet';
+$locale['jb_116'] = 'FAQ er blevet opdateret';
+$locale['jb_117'] = 'FAQ er blevet tilføjet';
+$locale['jb_118'] = 'Spørgsmål';
+$locale['jb_119'] = 'Du skal indtaste Spørgsmål';
+$locale['jb_120'] = 'Svar';
+$locale['jb_121'] = 'Du skal indtaste Svar';
+$locale['jb_122'] = 'Ingen FAQs';
+$locale['jb_123'] = 'Job titel';
+$locale['jb_124'] = 'Du skal indtaste Jobtitel';
+$locale['jb_125'] = 'Jobbeskrivelse';
+$locale['jb_126'] = 'Du skal indtaste Jobbeskrivelse';
+$locale['jb_127'] = 'Kategori';
+$locale['jb_128'] = 'Du skal vælge kategori';
+$locale['jb_129'] = 'Ikke valgt';
+$locale['jb_130'] = 'Placering';
+$locale['jb_131'] = 'Kategori er blevet slettet';
+$locale['jb_132'] = 'Kategori er blevet opdatret';
+$locale['jb_133'] = 'Kategori er blevet tilføjet';
+$locale['jb_134'] = 'Kategorinavn';
+$locale['jb_135'] = 'Du skal indtaste Kategorinavn';
+$locale['jb_136'] = 'Ingen kategorier';
+$locale['jb_137'] = 'Placeringen kan ikke slettes<br/>Der er job knyttet til denne placering';
+$locale['jb_138'] = 'Placeringen er blevet slettet';
+$locale['jb_139'] = 'Placeringen er blevet opdateret';
+$locale['jb_140'] = 'Placeringen er blevet tilføjet';
+$locale['jb_141'] = 'Du skal indtaste placeringsnavn';
+$locale['jb_142'] = 'Ingen placeringer';
+$locale['jb_143'] = 'Ansøger er blevet slettet';
+$locale['jb_144'] = 'Job';
+$locale['jb_145'] = 'Mail';
+$locale['jb_146'] = 'Telefon';
+$locale['jb_147'] = 'Hørt om os fra';
+$locale['jb_148'] = 'Praktik';
+$locale['jb_149'] = 'Besked';
+$locale['jb_150'] = 'Intern fejl';
+$locale['jb_151'] = 'Beskeden er sendt';
+$locale['jb_152'] = 'Emne';
+$locale['jb_153'] = 'Send mail';
+$locale['jb_154'] = 'Tid';
+$locale['jb_155'] = 'Navn';
+$locale['jb_156'] = 'CV';
+$locale['jb_157'] = 'Job';
+$locale['jb_158'] = 'Status';
+$locale['jb_159'] = 'Handlinger';
+$locale['jb_160'] = 'Åben';
+$locale['jb_161'] = 'Ubesvaret';
+$locale['jb_162'] = 'Besvaret';
+$locale['jb_163'] = 'Ingen ansøgere';
+$locale['jb_164'] = 'Indstillinger er blevet opdateret';
+$locale['jb_165'] = 'Mail fra / Svar til';
+$locale['jb_166'] = 'CV filstørrelse';
+$locale['jb_167'] = 'CV filtyper';
+$locale['jb_167a'] = 'Påkrævet CV';
+$locale['jb_168'] = 'Captcha';
+$locale['jb_169'] = 'Captcha er ikke valideret';
+$locale['jb_170'] = 'Ukendt fejl';
+$locale['jb_171'] = 'Ansøgning er sendt';
+$locale['jb_172'] = 'Ansøg';
+$locale['jb_173'] = 'Navn';
+$locale['jb_174'] = 'Dit navn';
+$locale['jb_175'] = 'Efternavn';
+$locale['jb_176'] = 'Dit efternavn';
+$locale['jb_177'] = 'Curriculum Vitae';
+$locale['jb_178'] = 'Hvad du udmærker dig i, og hvad du nyder?';
+$locale['jb_179'] = 'Hvordan hørte du om os?';
+$locale['jb_180'] = 'Ven';
+$locale['jb_181'] = 'Internet';
+$locale['jb_182'] = 'Annonce';
+$locale['jb_183'] = 'Placering';
+$locale['jb_184'] = 'Jeg er interesseret i en praktikplads for studerende';
+$locale['jb_185'] = 'Send';
+$locale['jb_186'] = 'Indtast valideringskode';
+$locale['jb_187'] = 'ledig stilling | ledige stillinger';
+$locale['jb_188'] = 'Mere';
+$locale['jb_189'] = 'N/A';
+$locale['jb_190'] = 'Ingen ledige stillinger';
+$locale['jb_191'] = 'Der er ingen kategorier';
+$locale['jb_192'] = 'Vi leder efter';
+$locale['jb_193'] = 'Jeg er interesseret i denne stilling';
+$locale['jb_194'] = 'Tilbage til listen over stillinger';
+$locale['jb_195'] = 'Ansøg om dette job';
+$locale['jb_196'] = 'Upload venligst CV';
