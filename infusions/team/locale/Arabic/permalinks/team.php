00cd6a8b1ca0975185ad9307ce7eb821a743456a
RobiNN <kelcakrobo@gmail.com>
2022-01-26 14:43:47 +0100

    Update

---
+++ infusions/team/locale/Arabic/permalinks/team.php
@@ -0,0 +1,19 @@
+<?php
+/*-------------------------------------------------------+
+| PHPFusion Content Management System
+| Copyright (C) PHP Fusion Inc
+| https://phpfusion.com/
++--------------------------------------------------------+
+| Filename: team.php
+| Author: Khamis Alqutob - PHPFusion-ar.xyz
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['pl_team_title'] = 'فريق العمل';
+$locale['pl_team_desc'] = 'تفعيل الروابط الثابتة لفريق العمل على الموقع.';
